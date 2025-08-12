cf9795dadaa1f90e35381bb7d43a9ceb06290fa6
ASIF RAZA <158028070+itx-asif@users.noreply.github.com>
2025-08-12 14:10:46 +0500

    Update sendToken.js

--- backend/util/sendToken.js
+++ backend/util/sendToken.js
@@ -5,7 +5,8 @@ const sendToken = (user, statuscode, res, message) => {
 
     const options = {
         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-        secure: true, // or true if using HTTPS
+        secure: true,
+        httpOnly: false,       // Prevent JS access // or true if using HTTPS
         sameSite: "none",
     }
 
