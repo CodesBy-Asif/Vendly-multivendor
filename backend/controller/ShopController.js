9bc1c95badfb7c9016f1e1984a841e14872646ea
ASIF RAZA <158028070+itx-asif@users.noreply.github.com>
2025-08-19 07:13:56 +0500

    updated error

--- backend/controller/ShopController.js
+++ backend/controller/ShopController.js
@@ -82,12 +82,7 @@ router.post('/register', upload.single("logo"), catchAsyncError(async (req, res,
                 <p>Ignore if not requested.</p>
             `,
         });
-        const cookieOptions = {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        };
+
 
         res.status(201).json({
             message: "Shop registered successfully. Please verify via email.",
